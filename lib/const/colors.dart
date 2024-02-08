0abeb82993413fb9ea9aac23b22eced2ece72a91
Abdulrahman-Haji <77126546+ENG-CJ@users.noreply.github.com>
2024-02-08 12:39:19 +0300

    initial setup_2

---
+++ lib/const/colors.dart
@@ -0,0 +1,11 @@
+
+
+import 'dart:ui';
+
+const colors=<String,Color>{
+  "primary" : Color(0xff3652AD),
+  "secondary": Color(0xff280274),
+  "error": Color(0xffFF004D),
+  "warning": Color(0xffFF6868),
+
+};
